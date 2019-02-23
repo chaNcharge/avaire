d687cb3fbc05cbb227745044bdf6e0ef3ab15955
chaNcharge <chancharge@gmail.com>
2019-02-22 21:09:03 -0800

    Disable nsfw check

--- src/main/java/com/avairebot/commands/search/DuckDuckGoCommand.java
+++ src/main/java/com/avairebot/commands/search/DuckDuckGoCommand.java
@@ -87,10 +87,6 @@ public class DuckDuckGoCommand extends Command {
 
     @Override
     public boolean onCommand(CommandMessage context, String[] args) {
-        if (context.isGuildMessage() && !context.getChannel().isNSFW()) {
-            return sendErrorMessage(context, context.i18n("nsfwDisabled"));
-        }
-
         if (args.length == 0) {
             return sendErrorMessage(context, "errors.missingArgument", "query");
         }
